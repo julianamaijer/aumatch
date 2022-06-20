cb98fbe2c6ab0fb48a61e39852a043ac025c9c7d
Juliana Maijer <julianamaijer@gmail.com>
2022-06-19 23:16:23 -0300

    massa

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,4 +6,6 @@ INSERT INTO ong (id_ong, razao_social, cnpj, id_endereco) VALUES (2, 'ONG Cantin
 
 
 INSERT INTO animal (id_animal, nome, raca, idade, porte, tipo_animal, sexo, descricao, foto, id_ong) VALUES (1, 'Lulu', 'sem raça definida', 2, 'pequeno', 'cao','femea','O Lulu é um cachorro que adora brincar com crianças! Extremamente ativo, exige bastante atenção de seus tutores!', 'https://cdn2.thedogapi.com/images/ygzwOZd_L.jpg', 1);
-INSERT INTO animal (id_animal, nome, raca, idade, porte, tipo_animal, sexo, descricao, foto, id_ong) VALUES (2, 'Dengoso', 'sem raça definida', 5, 'medio', 'gato', 'macho','essa eh a descricao temp', 'https://cdn2.thecatapi.com/images/19t.gif', 2);
+INSERT INTO animal (id_animal, nome, raca, idade, porte, tipo_animal, sexo, descricao, foto, id_ong) VALUES (2, 'Fininho', 'sem raça definida', 2, 'pequeno', 'cao','femea','essa eh a descricao temp!', 'https://cdn2.thedogapi.com/images/SyYtQe5V7_150x150.jpg', 1);
+INSERT INTO animal (id_animal, nome, raca, idade, porte, tipo_animal, sexo, descricao, foto, id_ong) VALUES (3, 'Dengoso', 'sem raça definida', 5, 'medio', 'gato', 'macho','essa eh a descricao temp', 'https://cdn2.thecatapi.com/images/19t.gif', 2);
+INSERT INTO animal (id_animal, nome, raca, idade, porte, tipo_animal, sexo, descricao, foto, id_ong) VALUES (4, 'Pablo', 'sem raça definida', 5, 'medio', 'gato', 'macho','essa eh a descricao temp', 'https://cdn2.thecatapi.com/images/6Wf5q65oZ.jpg', 2);
